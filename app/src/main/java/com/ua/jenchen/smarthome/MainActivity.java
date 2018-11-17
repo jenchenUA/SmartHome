ef88091c4e82496e0a7b42ecd198246e9ece69ff
Eugene Zadoyannyy <eugene.zadoyannyy@gmail.com>
2018-11-17 14:22:38 +0200

    Initial commit

---
+++ app/src/main/java/com/ua/jenchen/smarthome/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ua.jenchen.smarthome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
